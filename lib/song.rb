846b16eb3baea5c1d506814f74687f07fb9a72fd
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-10 16:40:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,8 @@ class Song
   
   
   def artist_name=(name)
-    Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
   
   def self.all
