777ba51aab6c20cc8bdf76da3580f33d4ab14b4b
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-10 16:24:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
   
   def self.new_by_filename(filename)
     song = self.new(name = filename.split(" - ")[1])
-    song.artist = filename.split(" - ")[0]
+    song.artist = filename.split(" - ")[2]
     song
   end
   
