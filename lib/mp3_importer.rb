39cb56cb26b6ab9f8e03f019f2fbfbb6ecd5382a
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-10 18:02:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,6 +10,7 @@ class MP3Importer
   end
 
   def files
+    Dir["/path/to/search/*"]
     # loads all the mp3 files in the path directory
     # normalizes the file name to just the mp3 filename with no path
   end
